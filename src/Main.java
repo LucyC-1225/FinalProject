bace86c5c2b4509e91b3d7a553515997fa8d93ff
B_1E10_32 <sample@bths.edu>
2022-05-20 11:57:49 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         Manager m = new Manager();
         m.retrieve(); //retrieves previous data
